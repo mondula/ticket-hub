6260019928fb4e09b1b6b047a939f44b46c4e69c
Marten Manthey <martenmanthey1995@gmail.com>
2024-05-14 11:05:19 +0200

    block-template

--- ticket-hub/block-templates/single-ticket.php
+++ ticket-hub/block-templates/single-ticket.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="profile" href="http://gmpg.org/xfn/11">
-    <?php wp_head(); ?>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<?php
+	$header = do_blocks('<!-- wp:template-part {"slug":"header","tagName":"header"} /-->');
+    $footer = do_blocks('<!-- wp:template-part {"slug":"footer","tagName":"footer"} /-->')
+ 	?>
+	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-    <header>
-        <?php block_template_part( 'header' ) ?>
-    </header>
-    <main>
-        <div class="wrapper">
-            <?php echo do_shortcode('[ticket id="' . get_the_ID() . '"]'); ?>
-        </div>
-    </main>
-    <footer>
-        <?php block_template_part( 'footer' ) ?>
-    </footer>
-
-    <?php wp_footer(); ?>
+<?php wp_body_open(); ?>
+<div class="wp-site-blocks">
+<?php echo $header; ?>
+<main>
+    <div class="wrapper">
+        <?php echo do_shortcode('[ticket id="' . get_the_ID() . '"]'); ?>
+    </div>
+</main>
+<?php echo $footer; ?>
+</div>
+<?php wp_footer(); ?>
 </body>
 </html>
