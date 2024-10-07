1b28f1b7e45d1ce3d53d8c0524e07f9a4279b3a6
Marten Manthey <martenmanthey1995@gmail.com>
2024-10-07 12:01:44 +0200

    remove obsolete admin js for tickethub post status handling

--- ticket-hub/dist/js/ticket-hub-admin.js
+++ ticket-hub/dist/js/ticket-hub-admin.js
@@ -1,24 +1,5 @@
 /**
- * Admin post status script
- */
-
-jQuery(document).ready(function ($) {
-    console.log('thub_admin_vars', thub_admin_vars);
-    // Get the archived text from localized variables
-    var archivedText = thub_admin_vars.archived_text;
-
-    // Append the new status to the status selector in the edit post and quick edit screens
-    $("select[name='post_status']").append("<option value='thub_archive'>" + archivedText + "</option>");
-
-    // Check if the current post status is 'thub_archive' and update the selector and display
-    if (thub_admin_vars.post_status === 'thub_archive') {
-        $("select[name='post_status']").val('thub_archive');
-        $('#post-status-display').text(archivedText);
-    }
-});
-
-/**
- * Document Script
+ * Admin document post type script
  */
 
 jQuery(document).ready(function ($) {
@@ -60,7 +41,7 @@ jQuery(document).ready(function ($) {
     });
 });
 /**
- * Form editor tab Script
+ * Admin form editor tab script
  */
 
 jQuery(document).ready(function ($) {
@@ -116,4 +97,23 @@ jQuery(document).ready(function ($) {
 
     // Initial update of required field indices
     updateRequiredFieldIndices();
+});
+
+/**
+ * Admin ticket post type script
+ */
+
+jQuery(document).ready(function ($) {
+    console.log('thub_admin_vars', thub_admin_vars);
+    // Get the archived text from localized variables
+    var archivedText = thub_admin_vars.archived_text;
+
+    // Append the new status to the status selector in the edit post and quick edit screens
+    $("select[name='post_status']").append("<option value='thub_archive'>" + archivedText + "</option>");
+
+    // Check if the current post status is 'thub_archive' and update the selector and display
+    if (thub_admin_vars.post_status === 'thub_archive') {
+        $("select[name='post_status']").val('thub_archive');
+        $('#post-status-display').text(archivedText);
+    }
 });
